d08c5b7bec036967d04b233d6e8c620093597c7b
Hyungsuk Hong <aanoaa@gmail.com>
2016-06-17 13:17:29 +0900

    Add Root controller

---
+++ lib/OpenCloset/Share/Web/Controller/Root.pm
@@ -0,0 +1,16 @@
+package OpenCloset::Share::Web::Controller::Root;
+use Mojo::Base 'Mojolicious::Controller';
+
+=head1 METHODS
+
+=head2 index
+
+    # root.index
+    GET /
+
+=cut
+
+sub index {
+}
+
+1
