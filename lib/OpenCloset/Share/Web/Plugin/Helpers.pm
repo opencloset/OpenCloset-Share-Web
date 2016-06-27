79089aa73a544cb693a989de8be7bd3117220e37
Hyungsuk Hong <aanoaa@gmail.com>
2016-06-27 17:47:55 +0900

    Add helper agent

--- lib/OpenCloset/Share/Web/Plugin/Helpers.pm
+++ lib/OpenCloset/Share/Web/Plugin/Helpers.pm
@@ -2,6 +2,8 @@ package OpenCloset::Share::Web::Plugin::Helpers;
 
 use Mojo::Base 'Mojolicious::Plugin';
 
+use HTTP::Tiny;
+
 use OpenCloset::Schema;
 
 =encoding utf8
@@ -22,10 +24,27 @@ OpenCloset::Share::Web::Plugin::Helpers - opencloset share web mojo helper
 
 sub register {
     my ( $self, $app, $conf ) = @_;
+
+    $app->helper( agent => \&agent );
 }
 
 =head1 HELPERS
 
+=head2 agent
+
+    my $agent = $self->agent;    # HTTP::Tiny object
+    my $res   = $agent->get('/api/user/30000.json');
+
 =cut
 
-1
+sub agent {
+    my $self = shift;
+
+    my $agent  = __PACKAGE__;
+    my $cookie = $self->cookie('opencloset');
+    my $http   = HTTP::Tiny->new( default_headers => { agent => $agent, cookie => "opencloset=$cookie" } );
+
+    return $http;
+}
+
+1;
