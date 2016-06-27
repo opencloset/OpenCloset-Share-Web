f44ddb8a71b90b4fe2bead6f70a89c08ea54c52a
Hyungsuk Hong <aanoaa@gmail.com>
2016-06-27 16:41:14 +0900

    Add Measurement controller

---
+++ lib/OpenCloset/Share/Web/Controller/Measurement.pm
@@ -0,0 +1,14 @@
+package OpenCloset::Share::Web::Controller::Measurement;
+use Mojo::Base 'Mojolicious::Controller';
+
+=head1 METHODS
+
+=head2 index
+
+    GET /measurements
+
+=cut
+
+sub index { }
+
+1;
