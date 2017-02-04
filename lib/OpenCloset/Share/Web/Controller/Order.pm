2b6cb77d2046c9189d59df1693670d6b5abd270c
Hyungsuk Hong <aanoaa@gmail.com>
2017-02-04 14:59:09 +0900

    If select jacket, pair(suit) pants are also added to the order list

--- lib/OpenCloset/Share/Web/Controller/Order.pm
+++ lib/OpenCloset/Share/Web/Controller/Order.pm
@@ -335,12 +335,32 @@ sub update_order {
     }
 
     if ( my $code = delete $input->{clothes_code} ) {
-        my $detail = $order->order_details( { name => $JACKET } )->next;
-        if ($detail) {
-            $detail->update( { clothes_code => sprintf( '%05s', $code ) } );
+        my $clothes = $self->schema->resultset('Clothes')->find( { code => $code } );
+        unless ($clothes) {
+            $self->log->warn("Not found clothes code: $code");
         }
         else {
-            $self->log->info("Not found clothes_code: $code");
+            my $detail = $order->order_details( { name => $JACKET } )->next;
+            if ($detail) {
+                $detail->update( { clothes_code => sprintf( '%05s', $code ) } );
+            }
+            else {
+                $self->log->info("Not found clothes_code: $code");
+            }
+
+            if ( my $bottom = $clothes->bottom ) {
+                my $category = $bottom->category;
+                my $detail = $order->order_details( { name => $category } )->next;
+                if ($detail) {
+                    $detail->update( { clothes_code => sprintf( '%05s', $bottom->code ) } );
+                }
+                else {
+                    $self->log->info( "Not found clothes_code: " . $bottom->code );
+                }
+            }
+            else {
+                $self->log->warn("Not found bottom: $code");
+            }
         }
     }
 
