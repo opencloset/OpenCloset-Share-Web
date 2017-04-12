45e3c899c6e346c3ac9fba35a5aebf43aee3ebc0
Hyungsuk Hong <aanoaa@gmail.com>
2017-04-13 08:52:14 +0900

    Allow two choices of preferred colors

--- lib/OpenCloset/Share/Web/Controller/Order.pm
+++ lib/OpenCloset/Share/Web/Controller/Order.pm
@@ -110,7 +110,7 @@ sub create {
             wearon_date      => $wearon_date,
             target_date      => $dates->{target}->datetime(),
             user_target_date => $dates->{target}->datetime(),
-            pre_color        => $v->param('pre_color'),
+            pre_color        => join( ',', @{ $v->every_param('pre_color') } ),
             purpose          => $v->param('purpose'),
             additional_day   => $additional_day,
             misc             => $misc,
