57675926f03bce127708392e1582b8d0901bdc32
Hyungsuk Hong <aanoaa@gmail.com>
2017-04-12 16:56:53 +0900

    Bumpup version

--- lib/OpenCloset/Share/Web.pm
+++ lib/OpenCloset/Share/Web.pm
@@ -10,7 +10,7 @@ use Iamport::REST::Client;
 use OpenCloset::Schema;
 use OpenCloset::DB::Plugin::Order::Sale;
 
-use version; our $VERSION = qv("v0.0.11");
+use version; our $VERSION = qv("v0.0.12");
 
 has schema => sub {
     my $self   = shift;
