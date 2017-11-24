6b63a13f9f92c00baa3f8c7e62375257fd01feb2
Hyungsuk Hong <aanoaa@gmail.com>
2017-11-24 11:49:26 +0900

    Fixed Address-related errors in coupon validation

--- lib/OpenCloset/Share/Web/Controller/Coupon.pm
+++ lib/OpenCloset/Share/Web/Controller/Coupon.pm
@@ -29,24 +29,33 @@ sub validate {
     my ( $coupon, $err ) = $self->coupon_validate($code);
     return $self->error( 400, $err ) if $err;
 
-    ## 취업날개는 쿠폰은 입사면접 용도만을 허용 (#178)
     my $desc = $coupon->desc || '';
-    if ( $desc =~ m/^seoul-2017-2/ and $order->purpose ne '입사면접' ) {
-        return $self->error(
-            400,
-            "대여목적이 입사면접이 아니므로 취업날개 쿠폰을 이용할 수 없습니다. 다른목적으로 이용하길 원하는 경우 대여주문서를 다시 작성해주세요."
-        );
-    }
+    if ( $desc =~ m/^seoul-2017-2/ ) {
+        ## 취업날개는 쿠폰은 입사면접 용도만을 허용 (#178)
+        if ( $order->purpose ne '입사면접' ) {
+            return $self->error(
+                400,
+                "대여목적이 입사면접이 아니므로 취업날개 쿠폰을 이용할 수 없습니다. 다른목적으로 이용하길 원하는 경우 대여주문서를 다시 작성해주세요."
+            );
+        }
 
-    ## 주소지가 서울시인 사람만 취업날개 이용 가능하도록 제한 (#187)
-    # address2
-    my $addr = $order->user_address;
-    my $address2 = $addr->address2 || '';
-    if ( $desc =~ m/^seoul-2017-2/ and $address2 !~ m/^서울특별시/ ) {
-        return $self->error(
-            400,
-            "취업날개 온라인(택배)대여는 서울 지역으로만 배송이 가능합니다. 배송 받을 주소를 서울시 주소로 입력해주세요."
-        );
+        ## 주소지가 서울시인 사람만 취업날개 이용 가능하도록 제한 (#187)
+        my $address2;
+        if ( my $addr = $order->user_address ) {
+            $address2 = $addr->address2 || '';
+        }
+        else {
+            my $user      = $order->user;
+            my $user_info = $user->user_info;
+            $address2 = $user_info->address2 || '';
+        }
+
+        if ( $address2 !~ m/^서울특별시/ ) {
+            return $self->error(
+                400,
+                "취업날개 온라인(택배)대여는 서울 지역으로만 배송이 가능합니다. 배송 받을 주소를 서울시 주소로 입력해주세요."
+            );
+        }
     }
 
     my %columns = $coupon->get_columns;
